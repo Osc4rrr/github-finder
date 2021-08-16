a0f73abe77df31169535eb37301654c299e3f225
Oscar Diaz <os.diazl@alumnos.duoc.cl>
2021-08-16 15:22:56 -0400

    cmp, state and props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios'; 
+import Search from './components/users/Search';
 
 class App extends Component {
-  render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
+  
+  state = {
+    users:[], 
+    loading:false
+  }
 
+  // async componentDidMount(){
+  //   this.setState({loading:true}); 
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
+  //   this.setState({users: res.data, loading:false}); 
+  // }
+
+  searchUsers = async (text) => {
+    this.setState({loading:true});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
+    this.setState({users: res.data.items, loading:false}); 
+  }
+
+  //Clear users from state 
+  clearUsers = () => this.setState({users:[], loading:false}); 
+
+  render() {
+
+    const {users, loading} = this.state; 
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello  {showName && name}</h1>}
+        <Navbar title="Github Finder" icon="fab fa-github"/>
+
+        <div className="container">
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users}/>
+        </div>
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,36 @@
+import React from 'react'; 
+import Proptypes from 'prop-types';
+
+
+const UserItem = ({user:{avatar_url, login, html_url}}) => {
+    return (
+            <div className="card text-center">
+                <img 
+                    src={avatar_url} 
+                    alt="" 
+                    className="round-img" 
+                    style={{ width:'80px'}}
+                />
+
+                <h3>{login}</h3>
+                
+                <div>
+                    <a 
+                        href={html_url} 
+                        className="btn btn-dark btn-sm my-1"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                            More
+                    </a>
+                </div>
+            </div>
+        )
+    
+}
+
+UserItem.propTypes ={
+    user:Proptypes.object.isRequired
+}
+
+export default UserItem
